f3caa8a783380242afc1baa9e713dd34fd8e8576
LapysDev <lapysdev@gmail.com>
2020-12-12 01:05:42 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -3,13 +3,47 @@
 #include <cstdio>
 
 /* ... Lapys::Main(ArgumentList const); */
-class Primitive {};
-class Object : Primitive {};
-
+class Object final {};
 int main(void) {
-    std::printf("[Object, Object]: %s" "\r\n", Lapys::is_base<Object, Object>::value ? "true" : "false");
-    std::printf("[Object, Primitive]: %s" "\r\n", Lapys::is_base<Object, Primitive>::value ? "true" : "false");
-    std::printf("[Primitive, Object]: %s" "\r\n", Lapys::is_base<Primitive, Object>::value ? "true" : "false");
-    std::printf("[Primitive, Primitive]: %s" "\r\n", Lapys::is_base<Primitive, Primitive>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...)>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...)&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...)&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const volatile>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const volatile&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) const volatile&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) volatile>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) volatile&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (...) volatile&&>::value ? "true" : "false");
+
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (&)(...)>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (*)(...)>::value ? "true" : "false");
+
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...)>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...)&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...)&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const volatile>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const volatile&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) const volatile&&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) volatile>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) volatile&>::value ? "true" : "false");
+    std::printf("[...]: %s" "\r\n", Lapys::is_variadic<int (Object::**)(...) volatile&&>::value ? "true" : "false");
+
+    std::printf("[{*}]: %s" "\r\n", Lapys::is_same<
+        int (...),
+        typename Lapys::remove_pointer<
+        typename Lapys::remove_qualifiers<
+        typename Lapys::remove_specifications<
+            int (Object::**)(...) const
+        >::type
+        >::type
+        >::type
+    >::value ? "true" : "false");
+
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
