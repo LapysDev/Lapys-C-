a30d01e0b3b1c1b554f11a1a9b8f7a6fcfe99e45
LapysDev <lapysdev@gmail.com>
2020-12-16 16:22:50 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -4,18 +4,38 @@
 
 /* ... Lapys::Main(ArgumentList const); */
 int main(void) {
-    class Object final { public:
-        Lapys::property<Object, char const*> name;
-        Lapys::property<Object, char const*> subname;
-    } object = {"Lapys", "Gyrnet"};
+    class Object {};
 
-    {} std::cout << "[...]: " << object.name << std::endl;
-    {} std::cout << "[...]: " << +object.name << std::endl;
-    {} std::cout << "[...]: " << (object.name + 1) << std::endl;
-    {} std::cout << "[...]: " << (1 + object.name) << std::endl;
-    { ++object.name; } std::cout << "[...]: " << object.name << std::endl;
-    { object.name = "Jyrsper"; } std::cout << "[...]: " << object.name << std::endl;
-    { object.name = object.subname; } std::cout << "[...]: " << object.name << std::endl;
+    Lapys::shadow<int> integer = 2019;
+    Lapys::shadow<Object> object = {};
+
+    (void) object;
+    std::cout << "[...]: " << ++integer << std::endl;
+
+    // class Object final { public:
+    //     struct string_t {
+    //         private: mutable char const *value;
+    //         public:
+    //             constexpr string_t(char const value[]) noexcept : value{value} {}
+    //             // inline string_t& operator =(char const value[]) noexcept { return (this -> value = value), *this; }
+
+    //             constexpr operator char const*&(void) const noexcept { return const_cast<char const*&>(this -> value); }
+    //     };
+
+    //     Lapys::property_accessor<Object, string_t> name;
+    //     Lapys::property_accessor<Object, string_t> subname;
+
+    //     constexpr string_t& getName(void) const noexcept { return const_cast<string_t&>(static_cast<string_t const&>(this -> name)); }
+    //     constexpr string_t& getSubname(void) const noexcept { return const_cast<string_t&>(static_cast<string_t const&>(this -> subname)); }
+    // } object = {"Lapys", "Gyrnet"};
+
+    // {} std::cout << "[...]: " << object.name << std::endl;
+    // {} std::cout << "[...]: " << +object.name << std::endl;
+    // {} std::cout << "[...]: " << (object.name + 1) << std::endl;
+    // {} std::cout << "[...]: " << (1 + object.name) << std::endl;
+    // { ++object.name; } std::cout << "[...]: " << object.name << std::endl;
+    // { object.name = "Jyrsper"; } std::cout << "[...]: " << object.name << std::endl;
+    // { object.name = object.subname; } std::cout << "[...]: " << object.name << std::endl;
 
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
