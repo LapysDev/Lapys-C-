9f1673bc1a3e41652ea766cdb62bffb5ce78c53a
LapysDev <lapysdev@gmail.com>
2020-12-26 08:38:49 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -1,64 +1,17 @@
 /* Import */
 #include <iostream>
-#include <type_traits>
 #include "lapys.hpp" // Lapys
 
-// #define MUTABLE true
-// class Object final {
-//     public:
-//         class String { friend Object;
-//             private: char const *value;
-//             public:
-//                 constexpr inline String(char const value[]) noexcept : value{value} {}
-//                 constexpr inline String(String const& string) noexcept : value{string.value} {}
-//                 constexpr inline String(String&& string) noexcept : value{string.value} {}
-
-//                 constexpr inline String& operator =(String const& string) const noexcept { return this -> operator =(string.value); }
-//                 constexpr inline String& operator =(String&& string) const noexcept { return this -> operator =(string.value); }
-
-//                 template <typename type, typename base = typename std::enable_if<false == std::is_same<String, typename std::decay<type>::type>::value, String&>::type>
-//                 constexpr inline base& operator =(type&& value) const noexcept { return static_cast<void>(const_cast<String*>(this) -> value = value), const_cast<String&>(*this); }
-
-//                 #if MUTABLE
-//                   inline char const*& operator +(void) noexcept { return ++(this -> value); }
-//                 #else
-//                   constexpr inline char const* const& operator +(void) const noexcept { return this -> value; }
-//                 #endif
-
-//                 constexpr inline operator char const*&(void) const noexcept { return const_cast<char const*&>(this -> value); }
-//         };
-
-//         // String name;
-//         // Lapys::shadow<String> name;
-//         // Lapys::property<Object, String> name;
-//         Lapys::property_accessor<Object, String> name;
-
-//         constexpr inline Object(String const& name) noexcept : name{name} {}
-
-//         #if MUTABLE
-//             inline auto getName(void) noexcept -> decltype(static_cast<String*>(NULL) -> operator +()) { return this -> name.operator +(); }
-//         #else
-//             constexpr inline auto getName(void) const noexcept -> decltype(static_cast<String const*>(NULL) -> operator +()) { return this -> name.operator +(); }
-//         #endif
-// };
-
+class Object {};
 int main(void) /* ... Lapys::Main(ArgumentList const); */ {
-    class Number {
-        public:
-            int operator &(void) const { return 1337; }
-            operator int(void) const { return 0; }
-    };
+    Lapys::property<Object, int> dummy = 0x3C;
+    Lapys::property<Object, int> number = dummy;
 
-    std::cout << "[  ]: " << &Lapys::shadow<Number>() << std::endl;
-    std::cout << "[L ]: " << (Lapys::shadow<Number>() + 69) << std::endl;
-    std::cout << "[ R]: " << (69 + Lapys::shadow<Number>()) << std::endl;
-    std::cout << "[LR]: " << (Lapys::shadow<Number>() + Lapys::shadow<Number>()) << std::endl;
-    // Object object = {"@Lapys"};
+    // dummy = 0;
+    // number = dummy;
 
-    // std::cout << "[...]: " << '"' << object.name << '"' << std::endl;
-    // std::cout << "[...]: " << '"' << object.getName() << '"' << std::endl;
-    // std::cout << "[...]: " << '"' << +(object.name) << '"' << std::endl;
-    // std::cout << "[...]: " << '"' << const_cast<Lapys::property_accessor<Object, Object::String> const&>(object.name).operator +() << '"' << std::endl;
+    {}                 std::cout << "[...]: " << number << std::endl;
+    { number += 011; } std::cout << "[...]: " << number << std::endl;
 
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
