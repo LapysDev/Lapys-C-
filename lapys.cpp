f88e1aafaea5ba9315353a0e4ee24c4329d7a26f
LapysDev <lapysdev@gmail.com>
2021-01-04 09:55:30 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "lapys.hpp" // Lapys
 
+class Object {};
 int main(void) /* ... Lapys::Main(ArgumentList const); */ {
+    std::boolalpha(std::cout);
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
