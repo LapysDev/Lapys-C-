6c16c03bffb02330102c229d096c3682d94e58dd
LapysDev <lapysdev@gmail.com>
2021-01-11 12:16:28 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -10,5 +10,16 @@ class String { public: String(char const[], ...) {} };
 
 void Lapys::Main(... /* ArgumentList const */) {
     std::boolalpha(std::cout);
+
+    Lapys::Utility::shadow<int> const integer = 0x44; {
+        // would not work if `int const` was shadowed, instead
+        std::cout << "[int]: " << ++integer << std::endl;
+        std::cout << "[int]: " << integer++ << std::endl;
+
+        // test for incompatible operator overloads...
+        new Lapys::Utility::shadow<Object>();
+        new Lapys::Utility::shadow<String>("...");
+    }
+
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
