edf28505281bbfd4a905064cfecf2864c49544ce
LapysDev <lapysdev@gmail.com>
2021-01-26 00:37:15 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -12,24 +12,21 @@ struct String {
 
     inline char const* operator =(char const[]) const { return "\"Hello, World!\""; }
     inline char const* operator =(String const&) const { return "\"Lorem ipsum\""; }
+
+    inline int operator +(void) { return 420; }
     inline int operator +(void) const { return 42; }
 };
 
 /* Main */
-class Dummy {
-    private:
-        // static char const* get_string(String&) { return "Red sus..."; }
+class Immutable { public: Lapys::Utility::property<Immutable, String const> name; inline Immutable(...) : name() {} };
+class Modifiable { public: Lapys::Utility::property<Modifiable, String> name; inline Modifiable(...) : name() {} };
 
-    public:
-        Lapys::Utility::property<Dummy, String> name;
-        // Lapys::Utility::property<Dummy, String, char const* (String&), &get_string> name;
-        inline Dummy(...) : name() {}
-};
 void Lapys::Main(... /* ArgumentList const */) {
     std::boolalpha(std::cout);
 
     // ...
-    std::cout << "[...]: " << +(Dummy("Lapys").name) << std::endl;
+    std::cout << "[...]: " << +(Immutable("Lapys").name) << std::endl;
+    std::cout << "[...]: " << +(Modifiable("Lapys").name) << std::endl;
 
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
