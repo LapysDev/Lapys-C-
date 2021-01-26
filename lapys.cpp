44a82767a8d094a88d7a4a6f94c7eb4c81a27265
LapysDev <lapysdev@gmail.com>
2021-01-26 19:16:55 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -3,30 +3,8 @@
 #include <iostream>
 #include "lapys.hpp" // Lapys
 
-// ...
-enum Enumeration { ANY = 1337 };
-int function(void) { return 0x45; }
-union Object { public: int identity; inline Object(void) : identity(420) {} };
-struct String {
-    String(char const[] = NULL, ...) {}
-
-    inline char const* operator =(char const[]) const { return "\"Hello, World!\""; }
-    inline char const* operator =(String const&) const { return "\"Lorem ipsum\""; }
-
-    inline int operator +(void) { return 420; }
-    inline int operator +(void) const { return 42; }
-};
-
 /* Main */
-class Immutable { public: Lapys::Utility::property<Immutable, String const> name; inline Immutable(...) : name() {} };
-class Modifiable { public: Lapys::Utility::property<Modifiable, String> name; inline Modifiable(...) : name() {} };
-
 void Lapys::Main(... /* ArgumentList const */) {
     std::boolalpha(std::cout);
-
-    // ...
-    std::cout << "[...]: " << +(Immutable("Lapys").name) << std::endl;
-    std::cout << "[...]: " << +(Modifiable("Lapys").name) << std::endl;
-
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
