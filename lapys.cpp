bf275636ea3d709a8ea22869b976346dc75599a4
LapysDev <lapysdev@gmail.com>
2021-01-27 02:52:33 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 #include "lapys.hpp" // Lapys
 
-class Object {
-    private:
-        struct Integer { Integer(...) {} int operator +(void) { return 0x45; } };
-        struct NonInteger {};
+// class Object {
+//     private:
+//         struct Integer { Integer(...) {} int operator +(void) { return 0x45; } };
+//         struct NonInteger {};
 
-    public:
-        Lapys::Utility::accessor<Object, NonInteger> index;
-        inline Object(...) : index() {}
-};
+//     public:
+//         Lapys::Utility::accessor<Object, NonInteger> index;
+//         inline Object(...) : index() {}
+// };
 
 /* Main */
 void Lapys::Main(... /* ArgumentList const */) {
     std::boolalpha(std::cout);
-    std::cout << "[]: " << +Object().index << std::endl;
+    std::cout << "[...]: " << Lapys::Utility::is_immutable_operation<int, Lapys::Utility::operation::pre_add_assign, int>::value << std::endl;
+    std::cout << "[...]: " << Lapys::Utility::is_immutable_operation<int const, Lapys::Utility::operation::pre_add, int>::value << std::endl;
+    // std::cout << "[]: " << +Object().index << std::endl;
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
