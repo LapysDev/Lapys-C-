8e220991769c41e01eb82fbb48889dcf6b6408f7
LapysDev <lapysdev@gmail.com>
2021-02-04 02:05:14 +0100

    Update resource

--- lapys.cpp
+++ lapys.cpp
@@ -9,13 +9,16 @@ class Object {
         struct NonInteger {};
 
     public:
-        Lapys::Utility::accessor<Object, NonInteger> index;
+        Lapys::Utility::accessor<Object, Integer> index;
+
         inline Object(...) : index() {}
+        int getIndex(void) const { return +(this -> index); }
 };
 
 /* Main */
 void Lapys::Main(... /* ArgumentList const */) {
     std::boolalpha(std::cout);
-    std::cout << "[]: " << +Object().index << std::endl;
+    // std::cout << "[]: " << +Object().index << std::endl;
+    // std::cout << "[]: " << Object().getIndex() << std::endl;
     std::printf("%18s", "[PROGRAM EXECUTED]");
 }
