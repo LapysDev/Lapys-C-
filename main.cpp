a2bb1185d30b370bc4a1f9616e341d65c129dca3
LapysDev <oluwafunmitoo@gmail.com>
2022-02-26 22:57:07 +0000

    Update traits

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace Lapys;
 
 /* Main */
 int main() {
+  std::printf("[]: %4.5s" "\r\n", can_construct<int, int>::value ? "true" : "false");
+  std::printf("[]: %4.5s" "\r\n", can_construct<int, int, int>::value ? "true" : "false");
   // std::printf("%u", static_cast<unsigned>(widthof<bit<42u> >::value));
 }
 // void Lapys::Main(CommandLineArguments const& arguments) {
