9eab1b5a1d7fa2a9b1d4bfcc9e10bb6b04add35e
LapysDev <oluwafunmitoo@gmail.com>
2022-03-01 23:33:11 +0000

    Update traits

--- main.cpp
+++ main.cpp
@@ -6,13 +6,8 @@ using namespace Lapys;
 
 /* Main */
 int main() {
-  std::printf("int(int)     : %4.5s" "\r\n", can_construct<int, int>     ::value ? "true" : "false");
-  std::printf("int(int, int): %4.5s" "\r\n", can_construct<int, int, int>::value ? "true" : "false");
-  std::printf("int + int          : %4.5s" "\r\n", can_operate<add, int, int>           ::value ? "true" : "false");
-  std::printf("++int              : %4.5s" "\r\n", can_operate<pre_increment, int>      ::value ? "true" : "false");
-  std::printf("void (&)(int) (int): %4.5s" "\r\n", can_operate<call, void (&)(int), int>::value ? "true" : "false");
-  std::printf("void (int)    (int): %4.5s" "\r\n", can_operate<call, void (int),    int>::value ? "true" : "false");
-  // std::printf("%u", static_cast<unsigned>(widthof<bit<42u> >::value));
+  // std::printf("%u" "\r\n", static_cast<unsigned>(widthof<bit<42u> >  ::value));
+  std::printf("%u" "\r\n", static_cast<unsigned>(widthof<Lapys::byte>::value));
 }
 // void Lapys::Main(CommandLineArguments const& arguments) {
 //   static_cast<void>(arguments);
