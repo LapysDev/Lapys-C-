b2ca8e5edf9ff628513f2f969a8505d573c7d8ae
LapysDev <oluwafunmitoo@gmail.com>
2022-03-04 02:40:37 +0000

    Update traits

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ int main() {
   std::printf("nilof    [unsigned]: %lu" "\r\n", static_cast<unsigned long>(+nilof<unsigned const>   ::value));
   std::printf("nilof    [bit<31u>]: %lu" "\r\n", static_cast<unsigned long>(+nilof<bit<31u> const>   ::value));
   std::printf("nilof    [byte]    : %lu" "\r\n", static_cast<unsigned long>(+nilof<Lapys::byte const>::value));
-  // // std::puts("");
-  // // std::printf("widthof  [unsigned, ...]: %lu" "\r\n", static_cast<unsigned long>(+widthof<unsigned>     ::value));
-  // // std::printf("widthof  [unsigned, 55u]: %lu" "\r\n", static_cast<unsigned long>(+widthof<unsigned, 55u>::value));
-  // // std::printf("widthof  [bit<31u>, ...]: %lu" "\r\n", static_cast<unsigned long>(+widthof<bit<31u> >    ::value));
-  // // std::printf("widthof  [byte, ...]    : %lu" "\r\n", static_cast<unsigned long>(+widthof<Lapys::byte>  ::value));
+  std::puts("");
+  std::printf("widthof  [unsigned, ...]: %lu" "\r\n", static_cast<unsigned long>(+widthof<unsigned>     ::value));
+  std::printf("widthof  [unsigned, 55u]: %lu" "\r\n", static_cast<unsigned long>(+widthof<unsigned, 55u>::value));
+  std::printf("widthof  [bit<31u>, ...]: %lu" "\r\n", static_cast<unsigned long>(+widthof<bit<31u> >    ::value));
+  std::printf("widthof  [byte, ...]    : %lu" "\r\n", static_cast<unsigned long>(+widthof<Lapys::byte>  ::value));
 }
 // void Lapys::Main(CommandLineArguments const& arguments) {
 //   static_cast<void>(arguments);
